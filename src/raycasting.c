a774d436ea3de74d0382902fa3558f88e0c5860e
Boussard Fanny <boussard.fanny@gmail.com>
2024-06-20 06:59:17 +0200

    started to refactor

--- src/raycasting.c
+++ src/raycasting.c
@@ -49,7 +49,21 @@ void raycasting(t_data *img)
 	double pos_X;
 	double pos_Y;
 	double dir_X;
-	double dir_Y; 
+	double dir_Y;
+	double plane_X;
+	double plane_Y;
+	double ray_Dir_X;
+	double ray_Dir_Y;
+	double camera_X;
+	int map_X;
+	int map_Y;
+	int side;
+//	double deltaDist_X;
+//	double deltaDist_Y;
+//	double perp_WallDist;
+//	int step_X;
+//	int step_Y;
+	int hit;
 
 	int w;
 	int h;
@@ -60,7 +74,7 @@ void raycasting(t_data *img)
 
 //initial position vector
 	pos_X = 22;
-	pos_X = 12;
+	pos_Y = 12;
 
 //initial direction vector
 	dir_X = -1;
@@ -74,50 +88,49 @@ void raycasting(t_data *img)
 	while (x < w)
 	{
 		//calculate ray position and direction
-		double cameraX = 2 * x / (double)w - 1; //x-coordinate in camera space
-		double rayDirX = dirX + planeX * cameraX;
-		double rayDirY = dirY + planeY * cameraX;
+		camera_X = 2 * x / (double)w - 1; //x-coordinate in camera space
+		ray_Dir_X = dir_X + plane_X * camera_X;
+		ray_Dir_Y = dir_Y + plane_Y * camera_X;
 
 		//which box of the map we're in
-		int mapX = (int)posX;
-		int mapY = (int)posY;
+		map_X = (int)pos_X;
+		map_Y = (int)pos_Y;
 
 		//length of ray from current position to next x or y-side
 		double sideDistX;
 		double sideDistY;
 
 		//length of ray from one x or y-side to next x or y-side
-		double deltaDistX = (rayDirX == 0) ? 1e30 : fabs(1 / rayDirX);
-		double deltaDistY = (rayDirY == 0) ? 1e30 : fabs(1 / rayDirY);
+		double deltaDistX = (ray_Dir_X == 0) ? 1e30 : fabs(1 / ray_Dir_X);
+		double deltaDistY = (ray_Dir_Y == 0) ? 1e30 : fabs(1 / ray_Dir_Y);
 		double perpWallDist;
 
 		//what direction to step in x or y-direction (either +1 or -1)
 		int stepX;
 		int stepY;
 
-		int hit = 0; //was there a wall hit?
-		int side; //was a NS or a EW wall hit?
+		hit = 0; //was there a wall hit?
 
 		//calculate step and initial sideDist
-		if (rayDirX < 0)
+		if (ray_Dir_X < 0)
 		{
 			stepX = -1;
-			sideDistX = (posX - mapX) * deltaDistX;
+			sideDistX = (pos_X - map_X) * deltaDistX;
 		}
 		else
 		{
 			stepX = 1;
-			sideDistX = (mapX + 1.0 - posX) * deltaDistX;
+			sideDistX = (map_X + 1.0 - pos_X) * deltaDistX;
 		}
-		if (rayDirY < 0)
+		if (ray_Dir_Y < 0)
 		{
 			stepY = -1;
-			sideDistY = (posY - mapY) * deltaDistY;
+			sideDistY = (pos_Y - map_Y) * deltaDistY;
 		}
 		else
 		{
 			stepY = 1;
-			sideDistY = (mapY + 1.0 - posY) * deltaDistY;
+			sideDistY = (map_Y + 1.0 - pos_Y) * deltaDistY;
 		}
 
 		//perform DDA
@@ -127,25 +140,25 @@ void raycasting(t_data *img)
 			if (sideDistX < sideDistY)
 			{
 				sideDistX += deltaDistX;
-				mapX += stepX;
+				map_X += stepX;
 				side = 0;
 			}
 			else
 			{
 				sideDistY += deltaDistY;
-				mapY += stepY;
+				map_Y += stepY;
 				side = 1;
 			}
 			//Check if ray has hit a wall
-			if (worldMap[mapX][mapY] > 0)
+			if (worldMap[map_X][map_Y] > 0)
 				hit = 1;
 		}
 
 		//Calculate distance to the point of impact
 		if (side == 0)
-			perpWallDist = (mapX - posX + (1 - stepX) / 2) / rayDirX;
+			perpWallDist = (map_X - pos_X + (1 - stepX) / 2) / ray_Dir_X;
 		else
-			perpWallDist = (mapY - posY + (1 - stepY) / 2) / rayDirY;
+			perpWallDist = (map_Y - pos_Y + (1 - stepY) / 2) / ray_Dir_Y;
 
 		//Calculate height of line to draw on screen
 		int lineHeight = (int)(h / perpWallDist);
