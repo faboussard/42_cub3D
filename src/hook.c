dbbcc5f3f14f88058796e956244077484a01018b
Melodie Bernard (from school) <melodie.bernard123@yahoo.fr>
2024-06-21 15:27:51 +0200

    not being able to close by ESC just because we didn't define movements yet is such a pain. Therefore, I dedide to comment only the parts about moving and uncomment the ESC part.

--- src/hook.c
+++ src/hook.c
@@ -26,10 +26,10 @@ int	close_window(t_data *img)
 	exit(1);
 }
 
-//int	key_hook(int keysym, t_data *img)
-//{
-//	if (keysym == XK_Escape)
-//		close_window(img);
+int	key_hook(int keysym, t_data *img)
+{
+	if (keysym == XK_Escape)
+		close_window(img);
 //	else if (keysym == XK_W || keysym == XK_w)
 //		move_up(img);
 //	else if (keysym == XK_A || keysym == XK_a)
@@ -38,5 +38,5 @@ int	close_window(t_data *img)
 //		move_down(img);
 //	else if (keysym == XK_D || keysym == XK_d)
 //		move_right(img);
-//	return (0);
-//}
+	return (0);
+}
