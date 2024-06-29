ce4bcfe80f8d0f52f734913d2f054272e167f75c
melodie <melodie.bernard123@yahoo.fr>
2024-06-29 17:44:39 +0200

    ajout strnjoin_free_both qui free les deux strings donnees

--- libft/src/ft_str/ft_strjoin.c
+++ libft/src/ft_str/ft_strjoin.c
@@ -40,42 +40,43 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	return (new_string);
 }
 
-static void	free_both_strjoin_str(char *s1, char *s2)
+static void	free_both_strjoin_str(char **s1, char **s2)
 {
 	if (*s1)
-		free(s1);
+	{
+		free(*s1);
+		*s1 = NULL;
+	}
 	if (*s2)
-		free(s2);
+	{
+		free(*s2);
+		*s2 = NULL;
+	}
 }
 
-char	*ft_strjoin_free_both(char const *s1, char const *s2)
+char	*ft_strjoin_free_both(char *s1, char *s2)
 {
 	size_t	total_len;
 	char	*dest;
 	int		x;
 	int		y;
 
-	if (!s2)
-	{
-		free_both_strjoin_str((char *)s1, (char *)s2);
+	if (!s1 || !s2)
 		return (NULL);
-	}
-	if (!s1)
-		return (ft_strdup(s2));
 	total_len = ft_strlen(s1) + ft_strlen(s2);
 	dest = (char *)malloc(sizeof(char) * (total_len + 1));
 	if (!dest)
 	{
-		free_both_strjoin_str((char *)s1, (char *)s2);
+		free_both_strjoin_str(&s1, &s2);
 		return (NULL);
 	}
 	x = -1;
 	y = 0;
 	while (s1[++x])
-		dest[x] = (char)s1[x];
+		dest[x] = s1[x];
 	while (s2[y])
-		dest[x++] = (char)s2[y++];
+		dest[x++] = s2[y++];
 	dest[x] = '\0';
-	free_both_strjoin_str((char *)s1, (char *)s2);
+	free_both_strjoin_str(&s1, &s2);
 	return (dest);
 }
