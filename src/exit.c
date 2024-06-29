70f86c43ec1d07e8a8c60ebee7b948c555aafb34
faboussa <boussard.fanny@gmail.com>
2024-06-29 12:25:26 +0200

    refacto + comments on raycasting

--- src/exit.c
+++ src/exit.c
@@ -12,22 +12,22 @@
 
 #include "cub3d.h"
 
-void	destroy_images(t_data *img)
-{
-	if (img->empty)
-		mlx_destroy_image(img->mlx, img->empty);
-	if (img->wall)
-		mlx_destroy_image(img->mlx, img->wall);
-	if (img->coll)
-		mlx_destroy_image(img->mlx, img->coll);
-	if (img->exit)
-		mlx_destroy_image(img->mlx, img->exit);
-	if (img->up)
-		mlx_destroy_image(img->mlx, img->up);
-	if (img->down)
-		mlx_destroy_image(img->mlx, img->down);
-	if (img->left)
-		mlx_destroy_image(img->mlx, img->left);
-	if (img->right)
-		mlx_destroy_image(img->mlx, img->right);
-}
+//void	destroy_images(t_data *img)
+//{
+//	if (img->empty)
+//		mlx_destroy_image(img->mlx, img->empty);
+//	if (img->wall)
+//		mlx_destroy_image(img->mlx, img->wall);
+//	if (img->coll)
+//		mlx_destroy_image(img->mlx, img->coll);
+//	if (img->exit)
+//		mlx_destroy_image(img->mlx, img->exit);
+//	if (img->up)
+//		mlx_destroy_image(img->mlx, img->up);
+//	if (img->down)
+//		mlx_destroy_image(img->mlx, img->down);
+//	if (img->left)
+//		mlx_destroy_image(img->mlx, img->left);
+//	if (img->right)
+//		mlx_destroy_image(img->mlx, img->right);
+//}
