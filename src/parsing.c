848ab038950e3757358ba53fb8757c9248541d4d
mbernard <mbernard@student.42lyon.fr>
2024-07-02 23:34:09 +0200

    tests to put the elements on the right places

--- src/parsing.c
+++ src/parsing.c
@@ -24,6 +24,57 @@ void   check_file_name(char *file)
 	}
 }
 
+static void	ft_free_tab(char **tab)
+{
+	size_t	i;
+
+	i = 0;
+	if (!tab || !*tab)
+		return ;
+	while (tab[i])
+	{
+		if (tab[i])
+			free(tab[i]);
+		i++;
+	}
+	free(tab);
+	tab = NULL;
+}
+
+static void	define_colors(t_data *cub)
+{
+	int		i;
+
+	i = 0;
+	while (i < 6)
+	{
+		if (ft_strncmp(cub->map.copy[i], "F ", 2) == 0)
+			cub->floor_color = cub->map.copy[i];
+		else if (ft_strncmp(cub->map.copy[i], "C ", 2) == 0)
+			cub->ceiling_color = cub->map.copy[i];
+		i++;
+	}
+}
+
+static void	define_textures_path(t_data *cub)
+{
+	int		i;
+
+	i = 0;
+	while (i < 6)
+	{
+		if (ft_strncmp(cub->map.copy[i], "NO ", 3) == 0)
+			cub->north_img = cub->map.copy[i];
+		else if (ft_strncmp(cub->map.copy[i], "SO ", 3) == 0)
+			cub->south_img = cub->map.copy[i];
+		else if (ft_strncmp(cub->map.copy[i], "WE ", 3) == 0)
+			cub->west_img = cub->map.copy[i];
+		else if (ft_strncmp(cub->map.copy[i], "EA ", 3) == 0)
+			cub->east_img = cub->map.copy[i];
+		i++;
+	}
+}
+
 void	parsing(t_data *cub, char *file)
 {
 //	int		fd;
@@ -32,6 +83,25 @@ void	parsing(t_data *cub, char *file)
 
 //	ret = 0;
 	define_map(&cub->map, file);
+	define_textures_path(cub);
+	define_colors(cub);
+	dprintf(2, "NORTH: %s\n", cub->map.copy[0]);
+	dprintf(2, "SOUTH: %s\n", cub->map.copy[1]);
+	dprintf(2, "WEST: %s\n", cub->map.copy[2]);
+	dprintf(2, "EAST: %s\n", cub->map.copy[3]);
+	cub->north_img = cub->map.copy[0];
+	cub->south_img = cub->map.copy[1];
+	cub->west_img = cub->map.copy[2];
+	cub->east_img = cub->map.copy[3];
+	dprintf(2, "north: %s\n", cub->north_img);
+	dprintf(2, "south: %s\n", cub->south_img);
+	dprintf(2, "west: %s\n", cub->west_img);
+	dprintf(2, "east: %s\n", cub->east_img);
+	ft_free_tab(cub->map.copy);
+	exit(1);
+
+	//check_map;
+
 //	fd = open(file, O_RDONLY);
 //	if (fd == -1)
 //	{
