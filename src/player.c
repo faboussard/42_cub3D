16d0ec16a564d8647f2565992d674b59f77c3fa6
Fanny BOUSSARD <boussard.fanny@gmail.com>
2024-07-09 22:21:14 +0200

    ajout 	{       		cub->player->pos_y = 2;       		cub->player->pos_x = 2;       	}
          	dans init_player_position

--- src/player.c
+++ src/player.c
@@ -66,8 +66,13 @@ void	init_player_position(t_data *cub)
 			break ;
 		y++;
 	}
-	cub->player->pos_y = (double)y;//je suis en dehors de la carte?
-	cub->player->pos_x = (double)x; //je suis en dehors de la carte?
+	cub->player->pos_y = (double)y;
+	cub->player->pos_x = (double)x;
+	if (y <= 1 || x <= 1)
+	{
+		cub->player->pos_y = 2;
+		cub->player->pos_x = 2;
+	}
 	init_player_eyes(cub, cub->map.grid[y][x]);
 }
 
