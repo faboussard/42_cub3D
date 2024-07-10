b6fd0319941059b9783eb75780742ea2a34fe73c
faboussa <boussard.fanny@gmail.com>
2024-07-10 11:48:45 +0200

    ajout init plane avec angle de 66 degres dansinit_player_eyes

--- src/move.c
+++ src/move.c
@@ -14,70 +14,61 @@
 
 void	move_forward(t_data *cub)
 {
-	double	dest_x;
-	double	dest_y;
+	int	x;
+	int	y;
 
-	dest_x = cub->player->pos_x + (cub->player->dir_x * MOVE);
-	dest_y = cub->player->pos_y + (cub->player->dir_y * MOVE);
-	if (cub->map.grid[(int)dest_x][(int)dest_y] 
-	&& cub->map.grid[(int)dest_x][(int)dest_y] != '1') // voir comment on traite les espaces
-	{
-		cub->player->pos_x = dest_x;
-		cub->player->pos_y = dest_y;
-	}
+	x = (int)cub->player->pos_x + (cub->player->dir_x * MOVE);
+	y = (int)cub->player->pos_y;
+	if (cub->map.grid[x][y] != '1')
+		cub->player->pos_x += cub->player->dir_x * MOVE;
+	x = (int)cub->player->pos_x;
+	y = (int)cub->player->pos_y + (cub->player->dir_y * MOVE);
+	if (cub->map.grid[x][y] && cub->map.grid[x][y] != '1')
+		cub->player->pos_y += cub->player->dir_y * MOVE;
 }
 
 void	move_backward(t_data *cub)
 {
-	double	dest_x;
-	double	dest_y;
+	int	x;
+	int	y;
 
-	dest_x = cub->player->pos_x + (cub->player->dir_x  * -1 * MOVE);
-	dest_y = cub->player->pos_y + (cub->player->dir_y * -1 * MOVE);
-	if (cub->map.grid[(int)dest_x][(int)dest_y] 
-	&& cub->map.grid[(int)dest_x][(int)dest_y] != '1')
-	{
-		cub->player->pos_x = dest_x;
-		cub->player->pos_y = dest_y;
-	}
+	x = (int)cub->player->pos_x + (cub->player->dir_x * -1 * MOVE);
+	y = (int)cub->player->pos_y;
+	if (cub->map.grid[x][y] != '1')
+		cub->player->pos_x += cub->player->dir_x * -1 * MOVE;
+	x = (int)cub->player->pos_x;
+	y = (int)cub->player->pos_y + (cub->player->dir_y * -1 * MOVE);
+	if (cub->map.grid[x][y] != '1')
+		cub->player->pos_y += cub->player->dir_y * -1 * MOVE;
 }
 
 void move_left(t_data *cub)
 {
-	double dest_x;
-	double dest_y;
+	int	x;
+	int	y;
 
-	dest_x = cub->player->pos_x + (cub->player->dir_y * MOVE);
-	dest_y = cub->player->pos_y - (cub->player->dir_x * MOVE);
-
-	if (cub->map.grid[(int)dest_x][(int)dest_y] 
-	&& cub->map.grid[(int)dest_x][(int)dest_y] != '1')
-	{
-		cub->player->pos_x = dest_x;
-	}
-		if (cub->map.grid[(int)dest_x][(int)dest_y] 
-	&& cub->map.grid[(int)dest_x][(int)dest_y] != '1')
-	{
-		cub->player->pos_y = dest_y;
-	}
+	x = (int)cub->player->pos_x + (cub->player->dir_x * MOVE);
+	y = (int)cub->player->pos_y;
+	if (cub->map.grid[x][y] != '1')
+		cub->player->pos_x += cub->player->dir_y * MOVE;
+	x = (int)cub->player->pos_x;
+	y = (int)cub->player->pos_y - (cub->player->dir_y * MOVE);
+	if (cub->map.grid[x][y] != '1')
+		cub->player->pos_y -= cub->player->dir_x * MOVE;
 }
 
 void move_right(t_data *cub)
 {
-	double dest_x;
-	double dest_y;
+		int	x;
+	int	y;
 
-	dest_x = cub->player->pos_x - (cub->player->dir_y * MOVE);
-	dest_y = cub->player->pos_y + (cub->player->dir_x * MOVE);
-	if (cub->map.grid[(int)dest_x][(int)dest_y] 
-	&& cub->map.grid[(int)dest_x][(int)dest_y] != '1')
-	{
-		cub->player->pos_x = dest_x;
-	}
-		if (cub->map.grid[(int)dest_x][(int)dest_y] 
-	&& cub->map.grid[(int)dest_x][(int)dest_y] != '1')
-	{
-		cub->player->pos_y = dest_y;
-	}
+	x = (int)cub->player->pos_x - (cub->player->dir_y * MOVE);
+	y = (int)cub->player->pos_y;
+	if (cub->map.grid[x][y] != '1')
+		cub->player->pos_x -= cub->player->dir_y * MOVE;
+	x = (int)cub->player->pos_x;
+	y = (int)cub->player->pos_y + (cub->player->dir_x * MOVE);
+	if (cub->map.grid[x][y] != '1')
+		cub->player->pos_y += cub->player->dir_x * MOVE;
 }
 
