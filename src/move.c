effc0525071903db5de5435182f5d00e1ea6acee
Fanny BOUSSARD <boussard.fanny@gmail.com>
2024-07-10 22:39:51 +0200

    parentheses added to convert all x to int and now it works perfectly

--- src/move.c
+++ src/move.c
@@ -17,14 +17,12 @@ void	move_forward(t_data *cub)
 	int	x;
 	int	y;
 
-	x = (int)cub->player->pos_x + (cub->player->dir_x * MOVE);
+	x = (int)(cub->player->pos_x + (cub->player->dir_x * MOVE));
 	y = (int)cub->player->pos_y;
-	printf("x is  %d\n", x);
-	printf("y is  %d\n", y);
 	if (cub->map.grid[y][x] != '1')
 		cub->player->pos_x += cub->player->dir_x * MOVE;
 	x = (int)cub->player->pos_x;
-	y = (int)cub->player->pos_y + (cub->player->dir_y * MOVE);
+	y = (int)(cub->player->pos_y + (cub->player->dir_y * MOVE));
 	if (cub->map.grid[y][x] != '1')
 		cub->player->pos_y += cub->player->dir_y * MOVE;
 }
@@ -34,12 +32,12 @@ void	move_backward(t_data *cub)
 	int	x;
 	int	y;
 
-	x = (int)cub->player->pos_x + (cub->player->dir_x * -1 * MOVE);
+	x = (int)(cub->player->pos_x + (cub->player->dir_x * -1 * MOVE));
 	y = (int)cub->player->pos_y;
 	if (cub->map.grid[y][x] != '1')
 		cub->player->pos_x += cub->player->dir_x * -1 * MOVE;
 	x = (int)cub->player->pos_x;
-	y = (int)cub->player->pos_y + (cub->player->dir_y * -1 * MOVE);
+	y = (int)(cub->player->pos_y + (cub->player->dir_y * -1 * MOVE));
 	if (cub->map.grid[y][x] != '1')
 		cub->player->pos_y += cub->player->dir_y * -1 * MOVE;
 }
@@ -49,12 +47,12 @@ void move_left(t_data *cub)
 	int	x;
 	int	y;
 
-	x = (int)cub->player->pos_x + (cub->player->dir_x * MOVE);
+	x = (int)(cub->player->pos_x + (cub->player->dir_x * MOVE));
 	y = (int)cub->player->pos_y;
 	if (cub->map.grid[y][x] != '1')
 		cub->player->pos_x += cub->player->dir_y * MOVE;
 	x = (int)cub->player->pos_x;
-	y = (int)cub->player->pos_y - (cub->player->dir_y * MOVE);
+	y = (int)(cub->player->pos_y - (cub->player->dir_y * MOVE));
 	if (cub->map.grid[y][x] != '1')
 		cub->player->pos_y -= cub->player->dir_x * MOVE;
 }
@@ -64,12 +62,12 @@ void move_right(t_data *cub)
 	int	x;
 	int	y;
 
-	x = (int)cub->player->pos_x - (cub->player->dir_y * MOVE);
+	x = (int)(cub->player->pos_x - (cub->player->dir_y * MOVE));
 	y = (int)cub->player->pos_y;
 	if (cub->map.grid[y][x] != '1')
 		cub->player->pos_x -= cub->player->dir_y * MOVE;
 	x = (int)cub->player->pos_x;
-	y = (int)cub->player->pos_y + (cub->player->dir_x * MOVE);
+	y = (int)(cub->player->pos_y + (cub->player->dir_x * MOVE));
 	if (cub->map.grid[y][x] != '1')
 		cub->player->pos_y += cub->player->dir_x * MOVE;
 }
